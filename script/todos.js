7605663f2463e3cef207f68ebff828f77fb6a6b1
Avik Bhattacharya <avikforandroid@gmail.com>
2017-07-20 23:58:38 +0530

    Third day commit, basic functionality and basic styling done. Animation remains

--- script/todos.js
+++ script/todos.js
@@ -2,7 +2,7 @@ $("ul").on("click","li",function(){
   $(this).toggleClass("completed");
 });
 
-$("span").on("click",function(e){
+$("ul").on("click","span",function(e){
   e.stopPropagation();
   $(this).parent().fadeOut(500,function(event){
     $(this).remove();
@@ -12,7 +12,10 @@ $("span").on("click",function(e){
 $("input").on("keypress",function(e){
   if(e.which === 13){
     var data = $(this).val();
-    $("ul").append("<li><span>X</span> " + data + "</li>");
+    $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i>&nbsp&nbsp</span> " + data + "</li>");
     $(this).val("");
   }
+});
+$("i").on("click",function(){
+  $("input").toggleClass("invisible");
 });
